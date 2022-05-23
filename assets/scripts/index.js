71a6e98371ba07bf22ec4e27927344cb820f4c47
KN-cod <ezra.njeru@student.moringaschool.com>
2022-05-23 05:57:34 +0300

    javascript stopwatch

---
+++ assets/scripts/index.js
@@ -0,0 +1,43 @@
+let startBtn = document.getElementById('start')
+let stopBtn = document.getElementById('stop')
+let resetBtn = document.getElementById('reset')
+let secnd = document.querySelector('.sec')
+let minit = document.querySelector('.min')
+let sum = 0
+let minitSum = 0
+let millisecond = 0
+let second = 0
+let minute = 0
+startBtn.addEventListener('click', ()=> {
+    if(sum !== 0){
+        clearInterval(sum)
+    }
+    sum = setInterval(displayTimer, 10)
+})
+stopBtn.addEventListener('click', ()=> {
+    clearInterval(sum)
+})
+resetBtn.addEventListener('click', ()=> {
+    clearInterval(sum)
+    millisecond = 0
+    second = 0
+    minute = 0
+    minit.innerHTML = '00:'
+    secnd.innerHTML = '00'
+})
+function displayTimer(){
+    millisecond +=10
+    console.log(millisecond)
+
+    if(millisecond == 1000){
+        millisecond = 0
+        second++
+        console.log("seconds: " + second)
+        secnd.innerHTML = second
+    }
+        if(second == 60){
+            second = 0
+            minute++
+            minit.innerHTML = '0' + minute +':'
+        }
+}
